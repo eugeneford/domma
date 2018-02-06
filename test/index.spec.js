7eb984d75b7fac8fb66c83d667a720d5d192c0d6
Eugene Ford <stmechanus@gmail.com>
2018-02-06 18:41:06 +0200

    update editor config

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,5 +1,9 @@
 import domma from '../src/domma';
 
+import './mutation-driver.spec';
+import './squash-mutations.spec';
+import './reference-map.spec';
+
 describe('domma', ()=>{
   it('is defined', ()=>{
     expect(domma).toBeDefined();
